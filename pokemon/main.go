db8339efebbc112f3f13b144920a06bbe920345a
nadiannis <annisanadianeyla@gmail.com>
2024-07-12 15:09:39 +0700

    refactor(pokemon): handle invalid id

--- pokemon/main.go
+++ pokemon/main.go
@@ -8,26 +8,10 @@ import (
 	"pokemon/model" // Import the model package containing the Pokemon struct
 )
 
-// catchProbability checks if catching is successful given a percentage rate
-func catchProbability(rate int) string {
-	if rate < 0 || rate > 100 {
-		return "Invalid rate. Please provide a rate between 0 and 100."
-	}
-
-	rand.Seed(time.Now().UnixNano()) // Seed the random number generator
-	chance := rand.Intn(100) + 1     // Generate a random number between 1 and 100
-
-	if chance <= rate {
-		return "SUCCESS, you caught it"
-	}
-	return "FAIL, it got away"
-}
-
-func main() {
-	now := time.Now()
-
-	// Create sample Pokemon
-	pokemons := []model.Pokemon{
+// Create sample Pokemon
+var (
+	now = time.Now()
+	pokemons = []model.Pokemon{
 		{ID: 1, Name: "Pikachu", Type: "Electric", CatchRate: 70, IsRare: false, RegisteredDate: now},
 		{ID: 2, Name: "Charmander", Type: "Fire", CatchRate: 40, IsRare: true, RegisteredDate: now.AddDate(0, -1, 0)},
 		{ID: 3, Name: "Bulbasaur", Type: "Grass/Poison", CatchRate: 10, IsRare: true, RegisteredDate: now.AddDate(0, -6, 0)},
@@ -43,6 +27,9 @@ func main() {
 		{ID: 13, Name: "Daniel", Type: "Dragon/Flying", CatchRate: 90, IsRare: false, RegisteredDate: now.AddDate(0, -1, 0)},
 		{ID: 14, Name: "Qowi", Type: "Electric", CatchRate: 60, IsRare: false, RegisteredDate: now.AddDate(0, -5, 0)},
 	}
+)
+
+func main() {
 
 	fmt.Println("Available Pokémon:")
 	for _, pokemon := range pokemons {
@@ -57,16 +44,21 @@ func main() {
 	for attempt < maxAttempt {
 		var id int
 		
-		fmt.Print("Enter Pokémon ID to attempt to catch: ")
+		fmt.Print("> Enter Pokémon ID to attempt to catch: ")
 		n, err := fmt.Scan(&id)
 		if err != nil {
-			fmt.Printf("err: %s\n", err)
+			fmt.Printf("%s, please try again\n\n", err)
 			continue
 		}
 
 		if n == 1 {
+			if !pokemonExists(id, pokemons) {
+				fmt.Printf("pokemon with ID %d does not exist, please try again\n\n", id)
+				continue
+			}
 			ids = append(ids, id)
 		}
+
 		attempt++
 	}
 
@@ -90,4 +82,28 @@ func main() {
 			fmt.Println("Invalid Pokémon ID. Please try again.")
 		}
 	}
+}
+
+func pokemonExists(id int, pokemons []model.Pokemon) bool {
+	for _, pokemon := range pokemons {
+		if id == pokemon.ID {
+			return true
+		}
+	}
+	return false
+}
+
+// catchProbability checks if catching is successful given a percentage rate
+func catchProbability(rate int) string {
+	if rate < 0 || rate > 100 {
+		return "Invalid rate. Please provide a rate between 0 and 100."
+	}
+
+	rand.Seed(time.Now().UnixNano()) // Seed the random number generator
+	chance := rand.Intn(100) + 1     // Generate a random number between 1 and 100
+
+	if chance <= rate {
+		return "SUCCESS, you caught it"
+	}
+	return "FAIL, it got away"
 }
